98cbf1020ca381b91b14bf9def795fb72c1abb67
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2020-04-04 15:18:31 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Headr from './components/header/header.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
